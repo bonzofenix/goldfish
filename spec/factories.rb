c8799a2e9a286d942e1887da52f22bd895d5df40
bonzofenix <bonzofenix@gmail.com>
2013-11-25 00:02:53 -0300

    posts factory

---
+++ spec/factories.rb
@@ -0,0 +1,27 @@
+
+FactoryGirl.define do
+  sequence(:title) {|n| "This is the post number #{n}" }
+  sequence(:name) {|n| "tag_#{n}" }
+
+  to_create do |instance|
+    if !instance.save
+      raise "Save failed for #{instance.class} error: #{instance.errors}"
+    end
+  end
+
+  factory :post do
+    title
+    short_description 'this is a short description'
+    content 'some content'
+  end
+
+
+  factory :tag do
+    name
+    trait :with_post do
+      after :create do |tag|
+        FactoryGirl.create_list :post, 1, tags: [ tag ]
+      end
+    end
+  end
+end
