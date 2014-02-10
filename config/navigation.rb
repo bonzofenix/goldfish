72ea8f55be17c8837573040e7cafdb21d9042b64
Alan Moran <bonzofenix@gmail.com>
2014-02-10 00:57:27 -0300

    minimize configs needed to run

--- config/navigation.rb
+++ config/navigation.rb
@@ -2,8 +2,10 @@ SimpleNavigation::Configuration.run do |navigation|
   navigation.selected_class = 'active'
   navigation.items do |primary|
     primary.dom_class = 'nav nav-pills nav-stacked'
-    $settings.sidebar_links.each do |l, i|
-      primary.item "sidebar_link_#{i}", l['text'], l['url']
+    if $settings.respond_to? :sidebar_links
+      $settings.sidebar_links.each do |l, i|
+        primary.item "sidebar_link_#{i}", l['text'], l['url']
+      end
     end
   end
 end
